def4446f8ebdb137a3f6c52a3a172efcd4bf3fcf
黄巍 <huan1043269994@gmail.com>
2018-04-29 10:56:05 +0800

    add a range of unility function

---
+++ index.js
@@ -0,0 +1,11 @@
+function isFunction(func) {
+  return typeof func === 'function';
+}
+
+function isObject(obj) {
+  return typeof obj === 'object';
+}
+
+function isArray(arr) {
+  return Array.isArray(arr);
+}
